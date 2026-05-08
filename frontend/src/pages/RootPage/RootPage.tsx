b8826d7aa35a1e8ce57113bc973f51a86138bb98
ShevchenkoO <shevchenko.sasha2005@gmail.com>
2026-05-08 13:33:06 +0200

    added: loader component for specific pages

--- frontend/src/pages/RootPage/RootPage.tsx
+++ frontend/src/pages/RootPage/RootPage.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header/Header.tsx";
 import PasteItems from "@/components/PasteItems/PasteItems";
 import { useGetPasteQuery } from "@/services/pasteApi.ts";
 import {useAuth} from "@/hooks/useAuth.ts";
+import {Loader} from "@/components/Loader/Loader.tsx";
 
 const RootPage = () => {
     const {
@@ -12,13 +13,12 @@ const RootPage = () => {
         isError,
         error
     } = useGetPasteQuery();
-    const { user, isAuth } = useAuth()
+    if (isLoading) return <Loader/>;
     return (
         <>
             <Header />
 
             <main style={{ padding: "24px" }}>
-                {isLoading && <p>Loading...</p>}
 
                 {isError && (
                     <p style={{ color: "red" }}>
